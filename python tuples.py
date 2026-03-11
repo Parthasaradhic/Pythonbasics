5e85f18ec8a9cbafd5db117038419ae2357cdf07
Your Name <chowdamparthasaradhi61@gmail.com>
2026-03-11 11:08:02 +0530

    tuples

---
+++ python tuples.py
@@ -0,0 +1,21 @@
+thistuple  =["apple","graph","banana"]
+print(thistuple)
+
+
+thistuple =["apple","graph","banana"]
+print(len(thistuple))
+
+
+thistuple =["apple","graph","banana"]
+thistuple.append("lemon")
+print(thistuple)
+
+
+thistuple = ["apple","graph","berry"]
+thistuple.remove("apple")
+print(thistuple)
+
+
+thistuple = ["apple","graph","berry"]
+result = " ,".join("pineapple")
+print(thistuple)
